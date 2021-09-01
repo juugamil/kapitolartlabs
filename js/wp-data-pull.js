e4906fba01865f24910684a0194461606fe02115
Juugamil <steezprm@gmail.com>
2021-09-01 18:04:15 -0400

    Updated link in WP data pull js to a HTTPS instead of HTTP

--- js/wp-data-pull.js
+++ js/wp-data-pull.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
 	
 	// Call the /posts endpoint via the WordPress API
-	$.getJSON("http://kapitolartlabsdesign.wordpress.com/wp-json/", function (posts) {
+	$.getJSON("https://kapitolartlabsdesign.wordpress.com/wp-json/", function (posts) {
   		
   		// Loop through all the posts returned and console.log() each of
   		// their HTML content
