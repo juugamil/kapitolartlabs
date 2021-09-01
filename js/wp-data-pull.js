7b3708bf467698c91401748004a43929c49f7652
Juugamil <steezprm@gmail.com>
2021-09-01 17:41:53 -0400

    Updated all pages to have uncompressed JQuery

--- js/wp-data-pull.js
+++ js/wp-data-pull.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
 	
 	// Call the /posts endpoint via the WordPress API
-	$.getJSON("http://wpuat.com/massive/wp-json/wp/v2/posts", function (posts) {
+	$.getJSON("http://steezprm.wordpress.com/wp-json/wp/posts/", function (posts) {
   		
   		// Loop through all the posts returned and console.log() each of
   		// their HTML content
