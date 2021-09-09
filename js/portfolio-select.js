edccc909d25a1a735458325ddb5ea3cdf653af97
Juugamil <steezprm@gmail.com>
2021-09-09 18:32:24 -0400

    Updated portfolio-select.js

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -1,31 +1,31 @@
-$(function () {
-    $("#radiusSelect").change(function () {
-        var val = $(this).val();
-        if (val === "1") {
-            $("#category-branding").show();
-            $("#category-webdesign").hide();
-            $("#category-intdesign").hide();
-            $("#category-workshops").hide();
-        } else if (val === "2") {
-            $("#category-webdesign").show();
-            $("#category-branding").hide();
-            $("#category-intdesign").hide();
-            $("#category-workshops").hide();
-        } else if (val === "3") {
-            $("#category-intdesign").show();
-            $("#category-branding").hide();
-            $("#category-webdesign").hide();
-            $("#category-workshops").hide();
-        } else if (val === "4") {
-            $("#category-workshops").show();
-            $("#category-branding").hide();
-            $("#category-intdesign").hide();
-            $("#category-webdesign").hide();
-        } else if (val === "5") {
-            $("#category-webdesign").show();
-            $("#category-branding").show();
-            $("#category-intdesign").show();
-            $("#category-workshops").show();
-        };
-    });
-});
+$(document).ready(function () {
+            $("#radiusSelect").change(function () {
+                var val = $(this).val();
+                if (val === "1") {
+                    $("#category-branding").show();
+                    $("#category-webdesign").hide();
+                    $("#category-intdesign").hide();
+                    $("#category-workshops").hide();
+                } else if (val === "2") {
+                    $("#category-webdesign").show();
+                    $("#category-branding").hide();
+                    $("#category-intdesign").hide();
+                    $("#category-workshops").hide();
+                } else if (val === "3") {
+                    $("#category-intdesign").show();
+                    $("#category-branding").hide();
+                    $("#category-webdesign").hide();
+                    $("#category-workshops").hide();
+                } else if (val === "4") {
+                    $("#category-workshops").show();
+                    $("#category-branding").hide();
+                    $("#category-intdesign").hide();
+                    $("#category-webdesign").hide();
+                } else if (val === "5") {
+                    $("#category-webdesign").show();
+                    $("#category-branding").show();
+                    $("#category-intdesign").show();
+                    $("#category-workshops").show();
+                };
+            });
+        });
