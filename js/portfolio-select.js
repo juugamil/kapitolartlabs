433cf3e2be4a730e43a2bcf404804eb3f3d03d26
Juugamil <steezprm@gmail.com>
2021-09-27 00:44:03 -0400

    Updated portfolio selector

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -1,31 +1,11 @@
 $(document).ready(function () {
     $("#radiusSelect").change(function () {
         var val = $(this).val();
-        if (val === "0") {
-            $("#category-branding").show();
-            $("#category-webdesign").show();
-            $("#category-intdesign").show();
-            $("#category-workshops").show();
-        } else if (val === "1") {
-            $("#category-webdesign").hide();
-            $("#category-intdesign").hide();
-            $("#category-workshops").hide();
-            $("#category-branding").show();
-        } else if (val === "2") {
-            $("#category-branding").hide();
-            $("#category-intdesign").hide();
-            $("#category-workshops").hide();
-            $("#category-webdesign").show();
-        } else if (val === "3") {
-            $("#category-branding").hide();
-            $("#category-webdesign").hide();
-            $("#category-workshops").hide();
-            $("#category-intdesign").show();
-        } else if (val === "4") {
-            $("#category-branding").hide();
-            $("#category-intdesign").hide();
-            $("#category-webdesign").hide();
-            $("#category-workshops").show();
-        };
+        var valid = "#" + val; 
+        if (val === $('portfolio-card').attr('id')){
+            valid.addClass("visible");
+        } else {
+            $(".portfolio-card").addClass("hide");
+        }
     });
 });
