e7f4af5930b2ed310bff70bc529a4fb3c3034e40
Juugamil <steezprm@gmail.com>
2021-09-30 17:37:43 -0400

    Updated selector

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
             $(this).find("option:selected").each(function () {
                 var optionValue = $(this).attr("value");
                 if (optionValue) {
-                    $(".portfolio-card").not("#" + optionValue).hide();
-                    $("#" + optionValue).show();
+                    $(".portfolio-card").not("." + optionValue).hide();
+                    $("." + optionValue).show();
                 } else if (optionValue = "All") {
                     $(".portfolio-card").show();
                 }
