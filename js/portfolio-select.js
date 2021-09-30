2d84151692e1d6e943fbde54fdced0a668a1dcf2
Juugamil <steezprm@gmail.com>
2021-09-30 16:20:23 -0400

    Updated selector

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -1,16 +1,9 @@
 $(document).ready(function () {
     $("#radiusSelect").change(function () {
         var val = $(this).val();
-        if (val == "0") {
-            $("div.portfolio-card").show();
-        } else if (val == "1") {
-            $("#category-branding").show();
-        } else if (val == "2") {
-            $("#category-webdesign").show();
-        } else if (val == "3") {
-            $("#category-intdesign").show();
-        } else if (val == "4") {
-            $("#category-workshops").show();
+        if (val == $('.portfolio-card').attr('id')) {
+            $(val).show();
+            console.log(val);
         } else {
             $("div.portfolio-card").hide();
         };
