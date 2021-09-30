91553dde042995a05edb41393775c95a6529b60a
Juugamil <steezprm@gmail.com>
2021-09-30 17:26:24 -0400

    Updated selector

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -1,26 +1,15 @@
 $(document).ready(function () {
-    var div = document.getElementById('portfolio-stack');
-    var divs = div.getElementsByTagName('portfolio-card');
-    var divArray = [];
-    for (var i = 0; i < divs.length; i += 1) {
-        divArray.push(divs[i].innerHTML);
-    };
-
-    console.log(divArray);
-
-    $("#radiusSelect").change(function () {
-        var val = $(this).val();
-        console.log(divArray);
-
-        for (i = 0; i < divArray.length; i++) {
-            if (val == "All") {
-                return divArray[i];
-            } else if (val == divArray[i].attr('id')) {
-                console.log(divArray[i].attr(val));
-                return divArray[i].attr(val);
-            } else {
-                return "Nothing to display";
-            }
-        }
+    $(document).ready(function () {
+        $("#radiusSelect").change(function () {
+            $(this).find("option:selected").each(function () {
+                var optionValue = $(this).attr("value");
+                if (optionValue) {
+                    $(".portfolio-card").not("#" + optionValue).hide();
+                    $("#" + optionValue).show();
+                } else {
+                    $(".portfolio-card").hide();
+                }
+            });
+        }).change();
     });
 });
