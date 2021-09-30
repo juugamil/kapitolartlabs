6c8ac6c412645300184019512610a30c252f9fc9
Juugamil <steezprm@gmail.com>
2021-09-30 18:25:48 -0400

    Updated services toggle

--- js/services-toggle.js
+++ js/services-toggle.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.services-list-block').on('click', function () {
     $(this).children().removeClass("hide");
-    $('.services-list-block').not(this).children().addClass("hide");
+    $(this).not(".hide").addClass("hide");
   });
 });
