a11b4f80a3fef8fdb1203b3afb0fc2bbb4eae574
Juugamil <steezprm@gmail.com>
2021-09-30 16:16:53 -0400

    Updated selector

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -5,24 +5,14 @@ $(document).ready(function () {
             $("div.portfolio-card").show();
         } else if (val == "1") {
             $("#category-branding").show();
-            $("#category-webdesign").hide();
-            $("#category-intdesign").hide();
-            $("#category-workshops").hide();
         } else if (val == "2") {
             $("#category-webdesign").show();
-            $("#category-branding").hide();
-            $("#category-intdesign").hide();
-            $("#category-workshops").hide();
         } else if (val == "3") {
             $("#category-intdesign").show();
-            $("#category-branding").hide();
-            $("#category-webdesign").hide();
-            $("#category-workshops").hide();
         } else if (val == "4") {
             $("#category-workshops").show();
-            $("#category-branding").hide();
-            $("#category-intdesign").hide();
-            $("#category-webdesign").hide();
+        } else {
+            $("div.portfolio-card").hide();
         };
     });
 });
