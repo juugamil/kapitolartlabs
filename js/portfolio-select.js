7859fc69f1c5b46f93cc9bd09b32e94704739501
Juugamil <steezprm@gmail.com>
2021-09-30 15:52:32 -0400

    Updated slider.

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#radiusSelect').on('change', function () {
         var demovalue = $(this).val();
-        $("div.portfolio-card").addClass("hide");
-        $("#" + demovalue).removeClass("hide");
+        $("div.portfolio-card").toggleClass("hide");
+        $("#" + demovalue).toggleClass("show");
     });
 });
