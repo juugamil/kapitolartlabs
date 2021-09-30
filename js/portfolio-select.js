515e086610ba047b5bd0aac7438d02ad7e867426
Juugamil <steezprm@gmail.com>
2021-09-30 16:23:16 -0400

    Updated selector

--- js/portfolio-select.js
+++ js/portfolio-select.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#radiusSelect").change(function () {
         var val = $(this).val();
         if (val == $('.portfolio-card').attr('id')) {
-            $(val).show();
+            $("#" + val).show();
             console.log(val);
         } else {
             $("div.portfolio-card").hide();
