95e50b481d671fd0146d492939dc6dad2eba19bd
Juugamil <steezprm@gmail.com>
2021-10-27 18:39:07 -0400

    Created KA cover page locally.

--- js/shopify.js
+++
@@ -1,7 +0,0 @@
-import Client from 'shopify-buy';
-
-// Initializing a client to return content in the store's primary language
-const client = Client.buildClient({
-    domain: 'kapitol-art-labs-demo.myshopify.com',
-    storefrontAccessToken: 'your-storefront-access-token'
-});
