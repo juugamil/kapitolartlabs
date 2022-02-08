ca4ec97c1c2f88cf0e0b9a16111cc9cc329082ff
Juugamil <steezprm@gmail.com>
2022-02-08 13:40:44 -0500

    Updated slider

--- js/services-toggle.js
+++ js/services-toggle.js
@@ -2,5 +2,7 @@ $(document).ready(function () {
   $(".services-list-block").on('click', function () {
     $('.service-description').removeClass("show"); 
     $(this).children().addClass("show");
+    $('.image-slider').removeClass("show"); 
+    $(this).children().addClass("show");
   });
 });
