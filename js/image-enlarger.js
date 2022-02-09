99b219c3a9fdfae54edf92051e2b1a190eed110b
Juugamil <steezprm@gmail.com>
2022-02-09 11:46:43 -0500

    Added image enlarger to page.

---
+++ js/image-enlarger.js
@@ -0,0 +1,9 @@
+const imgs = document.querySelectorAll('.slide > img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
